e97270ef073a71133cb9ddb76879ea7f50f8deb4
Rana-Mudassir <mduasire38@gmail.com>
2023-04-26 00:16:03 +0500

    updated Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
-import View from './Components/View';
 import Rockets from './Components/Rockets';
+import Missions from './Components/Missions';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<View />} />
-        <Route path="/rockets" element={<Rockets />} />
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
       </Routes>
     </>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import '../Assets/css/navbar.css';
 
+const links = [{
+  id: 1,
+  name: 'Rockets',
+  pathName: '/',
+}, {
+  id: 2,
+  name: 'Missions',
+  pathName: '/missions',
+}];
+
 function Navbar() {
+  const location = useLocation();
+  const linkClasses = (pathName) => `link ${location.pathname === pathName ? 'active-link' : ''}`;
   return (
     <header>
       <nav className="navbar">
         <h1>Space Travelers Hub</h1>
         <div className="nav-space-between">
           <ul className="nav">
-            <li className="nav-link">
-              <NavLink to="/" className={window.location.pathname === '/' ? 'active' : ''}>View</NavLink>
-            </li>
-            <li>
-              <NavLink id="navItem" to="/rockets" className={window.location.pathname === '/rockets' ? 'active' : ''}>Rockets</NavLink>
-            </li>
+            {
+              links?.map((link) => (
+                <li key={link.id}>
+                  <NavLink to={`${link.pathName}`} className={linkClasses(link.pathName)}>{link.name}</NavLink>
+                </li>
+              ))
+            }
           </ul>
         </div>
       </nav>
