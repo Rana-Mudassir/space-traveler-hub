dcc46149ffcc7ce11134bb05a9439f4a9b14d363
Rana-Mudassir <mduasire38@gmail.com>
2023-04-27 01:01:40 +0500

    Add data with get missions api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Rockets from './Components/rockets/Rockets';
-import Missions from './Components/Missions';
+import Missions from './Components/missions/MissionsList';
 
 function App() {
   return (
